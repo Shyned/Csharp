3a58e6ae0f7a3807388bf3733d226c257de48d44
Shyned <eyedolarts@gmail.com>
2022-09-28 23:12:30 -0500

    Created Terminal app

---
+++ draft/draft/Program.cs
@@ -0,0 +1,9 @@
+﻿
+
+string characterName = "Bobby Hill";
+int characterAge = characterName.Length * 7;
+
+Console.WriteLine("There once was a man named " + characterName);
+Console.WriteLine("He was 35 years old");
+Console.WriteLine("He really liked the name " +characterName);
+Console.WriteLine("But didn't like being 70");
