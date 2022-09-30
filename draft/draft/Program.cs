5128673f653880716ce8da1b0a285b76a31bafa6
Shyned <eyedolarts@gmail.com>
2022-09-29 22:27:31 -0500

    Made a  terminal calculator game

--- draft/draft/Program.cs
+++ draft/draft/Program.cs
@@ -1,9 +1,7 @@
 ﻿
 
-string characterName = "Bobby Hill";
-int characterAge = characterName.Length * 7;
 
-Console.WriteLine("There once was a man named " + characterName);
-Console.WriteLine("He was 35 years old");
-Console.WriteLine("He really liked the name " +characterName);
-Console.WriteLine("But didn't like being 70");
+Console.WriteLine("Enter your age: ");
+string age = Console.ReadLine();
+Console.WriteLine("Hello " + age);
+Console.ReadLine();
