74792fa25c937b5a9bb0cca4dbed6ae668a1e07b
Shyned <eyedolarts@gmail.com>
2022-10-04 23:12:45 -0500

    conditional quiz

--- draft/draft/Program.cs
+++ draft/draft/Program.cs
@@ -1,17 +1,14 @@
 ﻿
 
-string color , noun, celebrity ;
 
-Console.Write("Enter a color : ");
-color = Console.ReadLine();
-Console.Write("Enter a noun : ");
-noun = Console.ReadLine();
-Console.Write("Enter a celebrity : ");
-celebrity = Console.ReadLine();
 
-Console.WriteLine("Roses are " + color);
-Console.WriteLine(noun +" are blue");
-Console.WriteLine("I love "+ celebrity);
-
+bool isMale = false;
+  if (isMale )
+    { Console.WriteLine("You are a male");
+}
+else
+{
+    Console.WriteLine("you are not male");
+}
 
 Console.ReadLine();
