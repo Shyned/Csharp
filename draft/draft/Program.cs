df10fc9b8c977fd9b93e6555638dc8d37d8d41eb
Shyned <eyedolarts@gmail.com>
2022-10-05 12:51:11 -0500

    wrote switch case examples to get the days of the week

--- draft/draft/Program.cs
+++ draft/draft/Program.cs
@@ -2,13 +2,28 @@
 
 
 
-bool isMale = false;
-  if (isMale )
-    { Console.WriteLine("You are a male");
-}
-else
-{
-    Console.WriteLine("you are not male");
-}
-
+Console.WriteLine(GetDay(37));
 Console.ReadLine();
+
+static string GetDay (int dayNum)
+{   string dayName;
+    
+    switch (dayNum)
+    {
+        case 0: 
+            dayName = "Sunday";
+            break;
+        case 1: 
+            dayName = "Monday";
+            break ;
+        case 2:
+            dayName = "Tuesday";
+            break;
+      default:
+            dayName = "please enter a number from 0 - 2";
+            break;
+
+    }
+
+    return dayName;
+}
