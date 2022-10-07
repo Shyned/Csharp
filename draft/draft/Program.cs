561ecc3615ac9e5b2c0369e43b207a3681ca3e8a
Shyned <eyedolarts@gmail.com>
2022-10-06 22:39:34 -0500

    created the book class

--- draft/draft/Program.cs
+++ draft/draft/Program.cs
@@ -2,28 +2,23 @@
 
 
 
-Console.WriteLine(GetDay(37));
-Console.ReadLine();
 
-static string GetDay (int dayNum)
-{   string dayName;
-    
-    switch (dayNum)
-    {
-        case 0: 
-            dayName = "Sunday";
-            break;
-        case 1: 
-            dayName = "Monday";
-            break ;
-        case 2:
-            dayName = "Tuesday";
-            break;
-      default:
-            dayName = "please enter a number from 0 - 2";
-            break;
+class Book
+{
+   public string title;
+   public string author;
+   public string pages;
+}
 
+static void Main(string[] args)
+{
+    Book book1 = new Book();
+    book1.title= "Harry Potter";
+    book1.author = "JK Rowlings";
+    book1.pages = 499;
+
+    Console.WriteLine(book1.Title);
+    Console.ReadLine();
     }
 
-    return dayName;
-}
+
